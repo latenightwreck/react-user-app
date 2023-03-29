0cf05c3eefa74ffccadb51ddaf2caa1e2d1e71e2
Morgan <morgan.b.armstrong@gmail.com>
2023-03-29 10:46:03 -0400

    Working with portals for modal. Should be top level

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
+import ReactDOM from 'react-dom';
 import Button from './Button';
 import Card from './Card';
 import classes from './ErrorModal.module.css';
 
 export default function ErrorModal(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onHandleError} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onHandleError}>
-            {props.buttonTitle || 'Close'}
-          </Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onHandleError} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          onHandleError={props.onHandleError}
+          title={props.title}
+          message={props.message}
+        />,
+        document.getElementById('modal-root')
+      )}
+    </>
+  );
+}
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onHandleError} />;
+};
+
+function Modal(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onHandleError}>
+          {props.buttonTitle || 'Close'}
+        </Button>
+      </footer>
+    </Card>
   );
 }
