70d1f2a338464b82f6f21e864e772082e924945b
Morgan <morgan.b.armstrong@gmail.com>
2023-03-28 20:29:29 -0400

    Basic validation on input fields

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,6 +9,12 @@ export default function AddUser(props) {
 
   const addUserEventHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
