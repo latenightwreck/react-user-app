5ca0c161ba45a6c2422f956808bab6c29afc10e2
Morgan <morgan.b.armstrong@gmail.com>
2023-03-28 21:42:51 -0400

    Add dynamic error handling when form is invalid

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 export default function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserEventHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Required Data Missing',
+        message: 'Please make sure that Name and Age are filled.',
+      });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredAge <= 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Please make sure Age is greater than or equal to 1.',
+      });
       return;
     }
     const userData = {
@@ -34,27 +44,40 @@ export default function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserEventHandler}>
-        <label htmlFor='name'>Name</label>
-        <input
-          id='name'
-          type='text'
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onHandleError={errorHandler}
         />
-        <label htmlFor='age'>Age</label>
-        <input
-          id='age'
-          type='number'
-          min='0'
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type='submit'>Submit</Button>
-        {/* <button type='submit'>Submit</button> */}
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserEventHandler}>
+          <label htmlFor='name'>Name</label>
+          <input
+            id='name'
+            type='text'
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor='age'>Age</label>
+          <input
+            id='age'
+            type='number'
+            min='0'
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type='submit'>Submit</Button>
+          {/* <button type='submit'>Submit</button> */}
+        </form>
+      </Card>
+    </div>
   );
 }
