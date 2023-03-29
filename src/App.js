5ecf11ce4ac2889510defc0c74096f449a85c921
Morgan <morgan.b.armstrong@gmail.com>
2023-03-28 20:43:32 -0400

    Added UserList functionality with static data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const users = [
+    { id: 1, name: 'Morgan', age: '32' },
+    { id: 2, name: 'Allison', age: '31' },
+  ];
   return (
     <div>
       <AddUser />
+      <UsersList users={users} />
     </div>
   );
 }
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,16 @@
+import Card from '../UI/Card';
+import classes from './UsersList.module.css';
+
+export default function UsersList(props) {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
