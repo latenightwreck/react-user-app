f1bd4e2779bb07dd4ef89f40ab775100d8f02772
Morgan <morgan.b.armstrong@gmail.com>
2023-03-29 10:26:58 -0400

    Using react's fragments. Similar to wrapper

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
-
   const usersListHandler = (userData) => {
-        setUsersList((prevUsers) => {
-          return [userData, ...prevUsers];
-        });
+    setUsersList((prevUsers) => {
+      return [userData, ...prevUsers];
+    });
   };
 
   const [usersList, setUsersList] = useState([]);
   return (
-    <div>
-      <AddUser onAddUser={usersListHandler}/>
+    <>
+      <AddUser onAddUser={usersListHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 }
 
